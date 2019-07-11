708b5b20ee61f8337407b5f6eedb183707cd698b
supanat <5906021631075@fitm.kmutnb.ac.th>
2019-07-11 11:49:21 +0700

    first commit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 
        <tr>
-         <td><a href="auto.php?exercise=hello&done=index.php">Hello World</a></td><td><a href="https://firebasestorage.googleapis.com/v0/b/python-c0103.appspot.com/o/1.pdf?alt=media&token=5c352b63-5fd5-4b16-8b9f-ac09b41db048">รายละเอียด</a></td>
+         <td><a href="1.php">Hello World</a></td><td><a href="https://firebasestorage.googleapis.com/v0/b/python-c0103.appspot.com/o/1.pdf?alt=media&token=5c352b63-5fd5-4b16-8b9f-ac09b41db048">รายละเอียด</a></td>
        </tr>
        <tr>
          <td><a href="auto.php?exercise=2.2&done=index.php">Exercise 2.2</a></td><td><a href="https://firebasestorage.googleapis.com/v0/b/python-c0103.appspot.com/o/1.pdf?alt=media&token=5c352b63-5fd5-4b16-8b9f-ac09b41db048">รายละเอียด</a></td>
